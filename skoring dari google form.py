d548b445484072204326705b894b04d0aa232028
mzaini30 <muhzaini30@gmail.com>
2018-01-11 18:55:14 +0000

    oke

--- skoring dari google form.py
+++ skoring dari google form.py
@@ -1,4 +1,6 @@
 fav = open("1. fav.txt", "r").read().splitlines()
+fav = [a.replace(" ", "").replace("\t", "") for a in fav]
 unfav = open("1. unfav.txt", "r").read().splitlines()
+unfav = [a.replace(" ", "").replace("\t", "") for a in unfav]
 print fav
 print unfav
