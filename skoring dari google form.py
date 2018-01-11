1bae9cf7a793b7136b1eb3c49a860aaeeb04a4f8
mzaini30 <muhzaini30@gmail.com>
2018-01-11 20:38:58 +0000

    oke

--- skoring dari google form.py
+++ skoring dari google form.py
@@ -10,27 +10,13 @@ unfav = unfav.replace("\n", ",").replace("\t", "").replace(" ", "")
 unfav = unfav.split(",")
 unfav = [int(a) - 1 for a in unfav]
 
-blueprint = [0] * 10
+blueprint = [None] * (len(fav) + len(unfav))
 for a in fav:
-	# blueprint[a] = "fav"
-	pass
+	blueprint[a] = "fav"
+for a in unfav:
+	blueprint[a] = "unfav"
 
-# blueprint[0] = "fav"
 print blueprint
-
-
-# fav = open("1. fav.txt", "r").read().splitlines()
-# fav = [a.replace(", ", ",").replace("\t", "") for a in fav]
-# fav = [a.split(",") for a in fav]
-
-# unfav = open("1. unfav.txt", "r").read().splitlines()
-# unfav = [a.replace(" ", "").replace("\t", "") for a in unfav]
-# unfav = [a.split(",") for a in unfav]
 
 print fav
 print unfav
-
-print len(fav)
-print len(unfav)
-
-# flat = [x for sublist in nested for x in sublist]
