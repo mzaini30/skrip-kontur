3569ca19ff1bfde959044760a22ea4f224106f23
mzaini30 <muhzaini30@gmail.com>
2018-01-11 20:42:08 +0000

    oke

--- skoring dari google form.py
+++ skoring dari google form.py
@@ -1,3 +1,5 @@
+import os
+
 # mengolah favorable dan unfavorable
 
 fav = open("1. fav.txt", "r").read()
@@ -20,3 +22,56 @@ print blueprint
 
 print fav
 print unfav
+
+# copas dari skrip sebelumnya
+
+data = open("data.txt", "r").read().splitlines()
+data = [a.split("\t") for a in data]
+
+skoring = []
+error = False
+for a, b in enumerate(data):
+	for n, x in enumerate(blueprint):
+		if blueprint[n] == "fav":
+			if data[a][n] == "Sangat Setuju":
+				skoring.append("4")
+			elif data[a][n] == "Setuju":
+				skoring.append("3")
+			elif data[a][n] == "Tidak Setuju":
+				skoring.append("2")
+			elif data[a][n] == "Sangat Tidak Setuju":
+				skoring.append("1")
+			else:
+				skoring.append("error")
+				error = True
+
+		elif blueprint[n] == "unfav":
+			if data[a][n] == "Sangat Setuju":
+				skoring.append("1")
+			elif data[a][n] == "Setuju":
+				skoring.append("2")
+			elif data[a][n] == "Tidak Setuju":
+				skoring.append("3")
+			elif data[a][n] == "Sangat Tidak Setuju":
+				skoring.append("4")
+			else:
+				skoring.append("error")
+				error = True
+
+		else:
+			print "Cek blueprint.txt"
+
+skoring = [skoring[i:i+len(blueprint)] for i in range(0,len(skoring),len(blueprint))]
+
+hasil = open("hasil.txt", "w")
+
+for n, x in enumerate(data):
+	for a, b in enumerate(blueprint):
+		hasil.write(skoring[n][a])
+		hasil.write("\t")
+	hasil.write("\n")
+
+if error == True:
+	print "Ada error. Cek hasil.txt"
+print "Selesai."
+os.system("pause")
