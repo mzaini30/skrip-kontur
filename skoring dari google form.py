d802afddbd80e307421e46b6b7e7d00c3d41a0a7
mzaini30 <muhzaini30@gmail.com>
2018-01-11 18:58:26 +0000

    oke

--- skoring dari google form.py
+++ skoring dari google form.py
@@ -1,6 +1,12 @@
 fav = open("1. fav.txt", "r").read().splitlines()
 fav = [a.replace(" ", "").replace("\t", "") for a in fav]
+fav = [a.split(",") for a in fav]
+
 unfav = open("1. unfav.txt", "r").read().splitlines()
 unfav = [a.replace(" ", "").replace("\t", "") for a in unfav]
+
 print fav
 print unfav
+
+
+# flat = [x for sublist in nested for x in sublist]
