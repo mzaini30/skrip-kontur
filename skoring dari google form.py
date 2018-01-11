a1a0a9fc6d24d7803931dfd38f7d6335838466cc
mzaini30 <muhzaini30@gmail.com>
2018-01-11 21:04:21 +0000

    oke

--- skoring dari google form.py
+++ skoring dari google form.py
@@ -3,12 +3,12 @@ import os
 # mengolah favorable dan unfavorable
 
 fav = open("1. fav.txt", "r").read()
-fav = fav.replace("\n", ",").replace("\t", "").replace(" ", "")
+fav = fav.replace("\n\n", ",").replace("\n", ",").replace(" ", "")
 fav = fav.split(",")
 fav = [int(a) - 1 for a in fav]
 
 unfav = open("1. unfav.txt", "r").read()
-unfav = unfav.replace("\n", ",").replace("\t", "").replace(" ", "")
+unfav = unfav.replace("\n\n", ",").replace("\n", ",").replace(" ", "")
 unfav = unfav.split(",")
 unfav = [int(a) - 1 for a in unfav]
 
